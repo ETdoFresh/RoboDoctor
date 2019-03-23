68199c9251baf6ad3d1adadf9a0f5af508ad3631
ETdoFresh <ETdoFresh@gmail.com>
2019-03-23 12:39:20 -0500

    Add CATBaseHuman model. Animate model while running. Add respawn area if fall off cliff.

---
+++ Assets/Scripts/AnimatorData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorData : MonoBehaviour
+{
+    public Animator animator;
+    public new Rigidbody rigidbody;
+
+    private void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        var scale = transform.localScale;
+        scale.z = Mathf.Sign(rigidbody.velocity.x) * Mathf.Abs(scale.z);
+        transform.localScale = scale;
+
+        animator.SetFloat("VelocityX", Mathf.Abs(rigidbody.velocity.x));
+    }
+}
---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public BoxCollider boxCollider;
+
+    void OnValidate()
+    {
+        boxCollider = boxCollider ?? GetComponent<BoxCollider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.transform.position = Vector3.zero;
+    }
+}
